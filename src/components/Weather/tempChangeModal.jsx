42468647550c2f8c92631c75d5300fa780e948ad
MrDeveloper8051 <mrdeveloper8051@gmail.com>
2023-04-02 01:46:33 +0530

    add some feature

---
+++ src/components/Weather/tempChangeModal.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+
+export const TempChange = ({ show, setShow }) => {
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Button variant="primary" onClick={handleShow}>
+        Launch demo modal
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body
+          style={{ display: "flex", flexDirection: "column", gap: "10px" }}
+        >
+          <Button style={{ width: "30%", margin: "auto" }}>Celcius</Button>
+          <Button style={{ width: "30%", margin: "auto" }}>Farenheight</Button>
+          <Button style={{ width: "30%", margin: "auto" }}>Kelvin</Button>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+};
--- src/components/Weather/weather.jsx
+++ src/components/Weather/weather.jsx
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { Card } from "react-bootstrap";
 
 import "./weather.css";
 
 import city from "../../assests/img/city.png";
+import { Card, Button } from "react-bootstrap";
+import { TempChange } from "./tempChangeModal";
 export const Weather = () => {
+  const [show, setShow] = useState(false);
   const [data, setData] = useState([]);
   const [sys, setSys] = useState([]);
+  const [temperature, setTemperature] = useState([]);
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(function (position) {
       // setlong(position.coords.longitude);
@@ -19,17 +22,23 @@ export const Weather = () => {
           `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=56a1b72457882c27f73c03f6543d0cf2`
         )
         .then((res) => {
-          // console.log({ res });
+          console.log({ res });
           setData(res.data);
           setSys(res.data.sys);
+          setTemperature(res.data.main);
         });
     });
   }, []);
 
+  const kelvintocelius = (val) => {
+    const calcius = (Number(val) - 273.15).toFixed(2);
+    return calcius;
+  };
+
   const timeConvert = (currentTimestamp, check) => {
     currentTimestamp = Date.now();
     let date;
-    if (check == "date") {
+    if (check === "date") {
       date = new Intl.DateTimeFormat("en-IN", {
         year: "numeric",
         month: "2-digit",
@@ -46,20 +55,63 @@ export const Weather = () => {
     return date;
   };
 
+  const handleDegreeChange = () => {
+    console.log("hello");
+    setShow(true);
+  };
   return (
     <>
       <div className="weather">
         {/* {console.log({data})} */}
         <div className="weather-city">
           <div style={{ display: "flex" }}>
-            <img src={city} height="45" alt="" />
-            <h1>{data.name}</h1>
+            {data.name && (
+              <>
+                <img src={city} height="45" alt="" />
+                <h1>{data.name}</h1>
+              </>
+            )}
           </div>
           <div>
-            <h2>Date ➡️ {timeConvert(sys.sunrise, "date")}</h2>
-            <h2>Time ➡️ {timeConvert(sys.sunrise)}</h2>
+            {sys.sunrise && (
+              <>
+                <h2>Date ➡️ {timeConvert(sys.sunrise, "date")}</h2>
+                <h2>Time ➡️ {timeConvert(sys.sunrise)}</h2>
+              </>
+            )}
           </div>
         </div>
+        <div>
+          <Card className="temp-card">
+            <div className="temp-change">
+              <Button
+                onClick={() => {
+                  handleDegreeChange();
+                }}
+              >
+                Change Temp Unit
+              </Button>
+            </div>
+            <div className="temp-div">
+              {temperature.temp && (
+                <span>
+                  <strong>Temp. :-</strong>
+                  {kelvintocelius(temperature.temp)} &#8451;
+                </span>
+              )}
+
+              <span>
+                <strong>Max. Temp. :-</strong>
+                {kelvintocelius(temperature.temp_min)} &#8451;
+              </span>
+              <span>
+                <strong>Min. Temp. :-</strong>
+                {kelvintocelius(temperature.temp_max)} &#8451;
+              </span>
+            </div>
+          </Card>
+        </div>
+        <TempChange show={show} setShow={setShow} />
       </div>
     </>
   );
